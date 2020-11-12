6768e958d304e9930db6a9a448c7f732a1d9d7ea
zubar ahmed za <zahmed.pk@gmail.com>
2020-11-12 06:42:11 +0500

    Initial Commit

---
+++ SoloSet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoloSet
+//
+//  Created by Zubair Ahmed on 12/11/2020.
+//  Copyright © 2020 Zubair Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
