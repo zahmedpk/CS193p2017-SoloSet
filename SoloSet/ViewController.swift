2c0d70de1812156612f5209f06c0f318234a31af
zubar ahmed za <zahmed.pk@gmail.com>
2020-11-12 12:13:24 +0500

    Add actions and outlets.

--- SoloSet/ViewController.swift
+++ SoloSet/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var scoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func cardButtonTouched(_ sender: UIButton) {
+    }
+    
+    @IBAction func dealThreeMoreCardsButtonTouched(_ sender: UIButton) {
+    }
+    
+    @IBAction func newGameButtonTouched(_ sender: UIButton) {
+    }
 }
-
